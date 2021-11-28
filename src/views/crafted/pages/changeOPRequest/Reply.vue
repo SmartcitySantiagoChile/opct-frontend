2b8fac42e0adf40410caee0fd931eb2922862a60
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-11-28 19:54:52 -0300

    Refactoring

--- src/views/crafted/pages/changeOPRequest/Reply.vue
+++ src/views/crafted/pages/changeOPRequest/Reply.vue
@@ -113,7 +113,6 @@ export default defineComponent({
         creator: store.getters.currentUserUrl,
         change_op_request: store.getters.getCurrentChangeOPRequestUrl
       }
-      console.log(params);
       store.dispatch(Actions.CREATE_CHANGE_OP_REQUEST_MESSAGE, params).then(function () {
         Swal.fire({
           text: "Mensaje publicado exitosamente",
