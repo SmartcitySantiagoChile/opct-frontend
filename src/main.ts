8cfef881306496d041edc7ce8cccced7cde1b084
cephe <cephei.1313@gmail.com>
2022-07-01 02:22:56 -0400

    se quitan dependencias no utilizadas

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,11 @@ import ElementPlus from "element-plus";
 import i18n from "@/core/plugins/i18n";
 
 //imports for app initialization
-import MockAdapter from "@/core/mock/MockService";
 import ApiService from "@/core/services/ApiService";
-import { initApexCharts } from "@/core/plugins/apexcharts";
 import { initInlineSvg } from "@/core/plugins/inline-svg";
 import { initVeeValidate } from "@/core/plugins/vee-validate";
 
 import "@/core/plugins/keenthemes";
-import "@/core/plugins/prismjs";
 import "bootstrap";
 
 const app = createApp(App);
@@ -27,7 +24,6 @@ app.use(router);
 app.use(ElementPlus);
 
 ApiService.init(app);
-initApexCharts(app);
 initInlineSvg(app);
 initVeeValidate();
 
