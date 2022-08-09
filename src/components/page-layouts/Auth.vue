86822b0d03d8bbc96d908899570d2ad69f394d3b
cephe <cephei.1313@gmail.com>
2022-08-09 01:28:54 -0400

    remove metronic logo

--- src/components/page-layouts/Auth.vue
+++ src/components/page-layouts/Auth.vue
@@ -4,12 +4,6 @@
   >
     <!--begin::Content-->
     <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
-      <!--begin::Logo-->
-      <a href="#" class="mb-12">
-        <img alt="Logo" src="/media/logos/logo-1.svg" class="h-45px" />
-      </a>
-      <!--end::Logo-->
-
       <router-view></router-view>
     </div>
     <!--end::Content-->
